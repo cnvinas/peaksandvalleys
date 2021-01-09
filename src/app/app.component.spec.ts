234d74d5cedf65af360a02a06b9b311227a5b7e3
Christian Viñas <christian.vinas@messagegears.com>
2021-01-09 08:02:37 -0300

    Clean project and created new unit tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
@@ -16,17 +17,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'landsOfPeaksAndValleys'`, () => {
+  it(`should have as main action button title 'Click to see the Peaks And Valleys'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('landsOfPeaksAndValleys');
+    expect(app.mainActionButtonTitle).toEqual('Click to see the Peaks And Valleys');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Click to see the Peaks And Valleys');
+    expect(compiled.querySelector('.content .mainBtn').textContent).toContain('Click to see the Peaks And Valleys');
   });
 
   it('should render Peaks', () => {
@@ -42,4 +43,31 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.valleys').textContent).toContain('Print Valleys');
   });
+    
+  it('should provide an array to build a castle and get peaks and valleys totals', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    let buildCastles = spyOn(app, 'buildACastle');
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    compiled.querySelector('.buildACastle').click();
+    expect(buildCastles).toHaveBeenCalled();
+  });
+
+  it('should create new Valleys', () => {
+    const arr = [86, 4, 53, 46, 24, 2, 70, 44, 95, 34];
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.checkValleys(arr, 0, app.valleysList);
+    expect(app.valleysList.length).toBeGreaterThan(0);
+  });
+
+  it('should create new Peaks', () => {
+    const arr = [86, 4, 53, 46, 24, 2, 70, 44, 95, 34];
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.checkValleys(arr, 0, app.peaksList);
+    expect(app.peaksList.length).toBeGreaterThan(0);
+  });
+  
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,17 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Click to see the Peaks And Valleys';
-
+  /* 
+    You can make the following assumptions:
+    ● You can always build a castle at the start of the array, provided it is non-empty
+    ● You can always build a castle at the end of the array, provided it is non-empty
+    ● We only want to build one castle per peak or valley.
+    ● A peak is an integer or series of integers that is above the immediately preceding and following
+    ints. For example, in the sequence [2,6,6,6,3] the sequence of three 6s is a peak.
+    ● A valley is an integer or series of integers that is below the immediately preceding and
+    following ints. For example, in the sequence [6,1,4] the "1" would be a valley.
+  */
+  mainActionButtonTitle = 'Click to see the Peaks And Valleys';
   peaks = 0;
   peaksList: number[] = [];
   valleys = 0;
@@ -17,97 +26,82 @@ export class AppComponent {
   printPeaks = "Print Peaks";
   printValleys = "Print Valleys";
 
-  constructor(private cdRef:ChangeDetectorRef) {}
+  constructor(private cdRef: ChangeDetectorRef) { }
 
-  castlesInAland = () => {
-    //write a function that reads that array and returns the number of
-    //castles that Aequilibrium should build on that stretch of land
-    //and provide a way to test it.
-  };
-
-  buildACastle = () => {
-    const landArray: number[] = Array.apply(null, Array(10)).map(function() { return Math.floor(Math.random() * 100 % 100); })
+  buildACastle() {
+    const landArray: number[] = Array.apply(null, Array(10)).map(function () { return Math.floor(Math.random() * 100 % 100); })
+    console.log(landArray);
     this.peaksList = this.valleysList = [];
-    //You can always build a castle at the start of the array, provided it is non-empty
-    const checkValleys = (landArray: number[], valleys: number, valleysList: number[]) => {
-      let lastValley: number;
-
-      const addValley = (land: number, valleys: number, valleysList: number[]) => {
-        valleys++;
-        lastValley = land;
-        valleysList.push(land);
-      };
-
-      landArray.forEach((land, index, array) => {
-        if (index === 0 && land < array[index + 1]) {
-          //check initial value
-          addValley(land, valleys, valleysList);
-        } else if (index === array.length - 1 && land < array[index - 1]) {
-          //check last value
-          addValley(land, valleys, valleysList);
-        } else if (
-          //check middle value
-          (land < array[index - 1] && land < array[index + 1]) ||
-          (land < array[index - 1] && land === array[index + 1]) ||
-          (land !== lastValley &&
-            land === array[index + 1] &&
-            land < array[index + 2])
-        ) {
-          addValley(land, valleys, valleysList);
-        }
-      });
-
-      this.printValleys = "ValleysList: " + valleysList + " Total of valleys: " + valleysList.length;
-      this.cdRef.detectChanges();
-    };
-
-    const checkPeaks = (landArray: number[], peaks: number, peaksList: number[]) => {
-      let lastPeak: number;
-      const addPeak = (land: number, peaks: number, peaksList: number[]) => {
-        peaks++;
-        lastPeak = land;
-        peaksList.push(land);
-      };
-
-      landArray.forEach((land, index, array) => {
-        if (index === 0 && land > array[index + 1]) {
-          //check initial value
-          addPeak(land, peaks, peaksList);
-        } else if (index === array.length - 1 && land > array[index - 1]) {
-          //check last value
-          addPeak(land, peaks, peaksList);
-        } else if (
-          //check middle value
-          (land > array[index - 1] && land > array[index + 1]) ||
-          (land > array[index - 1] && land === array[index + 1]) ||
-          (land !== lastPeak &&
-            land === array[index + 1] &&
-            land > array[index + 2])
-        ) {
-          addPeak(land, peaks, peaksList);
-        }
-      });
-
-      this.printPeaks = "PeaksList: " + peaksList + " Total of peaks: " + peaksList.length;
-      this.cdRef.detectChanges();
-    };
-
     landArray
-      ? checkValleys(landArray, this.valleys, this.valleysList)
+      ? this.checkValleys(landArray, this.valleys, this.valleysList)
       : console.log("Please provide a larger set of lands");
 
     landArray
-      ? checkPeaks(landArray, this.peaks, this.peaksList)
+      ? this.checkPeaks(landArray, this.peaks, this.peaksList)
       : console.log("Please provide a larger set of lands");
   };
 
-  /* 
-  You can make the following assumptions:
-  ● You can always build a castle at the start of the array, provided it is non-empty
-  ● You can always build a castle at the end of the array, provided it is non-empty
-  ● We only want to build one castle per peak or valley.
-  ● A peak is an integer or series of integers that is above the immediately preceding and following
-  ints. For example, in the sequence [2,6,6,6,3] the sequence of three 6s is a peak.
-  ● A valley is an integer or series of integers that is below the immediately preceding and
-  following ints. For example, in the sequence [6,1,4] the "1" would be a valley. */
+  checkPeaks(landArray: number[], peaks: number, peaksList: number[]) {
+    let lastPeak: number;
+    const addPeak = (land: number, peaks: number, peaksList: number[]) => {
+      peaks++;
+      lastPeak = land;
+      peaksList.push(land);
+    };
+
+    landArray.forEach((land, index, array) => {
+      if (index === 0 && land > array[index + 1]) {
+        //check initial value
+        addPeak(land, peaks, peaksList);
+      } else if (index === array.length - 1 && land > array[index - 1]) {
+        //check last value
+        addPeak(land, peaks, peaksList);
+      } else if (
+        //check middle value
+        (land > array[index - 1] && land > array[index + 1]) ||
+        (land > array[index - 1] && land === array[index + 1]) ||
+        (land !== lastPeak &&
+          land === array[index + 1] &&
+          land > array[index + 2])
+      ) {
+        addPeak(land, peaks, peaksList);
+      }
+    });
+
+    this.printPeaks = "PeaksList: " + peaksList + " Total of peaks: " + peaksList.length;
+    this.cdRef.detectChanges();
+  };
+
+  checkValleys(landArray: number[], valleys: number, valleysList: number[]) {
+    let lastValley: number;
+
+    const addValley = (land: number, valleys: number, valleysList: number[]) => {
+      valleys++;
+      lastValley = land;
+      valleysList.push(land);
+    };
+
+    landArray.forEach((land, index, array) => {
+      if (index === 0 && land < array[index + 1]) {
+        //check initial value
+        addValley(land, valleys, valleysList);
+      } else if (index === array.length - 1 && land < array[index - 1]) {
+        //check last value
+        addValley(land, valleys, valleysList);
+      } else if (
+        //check middle value
+        (land < array[index - 1] && land < array[index + 1]) ||
+        (land < array[index - 1] && land === array[index + 1]) ||
+        (land !== lastValley &&
+          land === array[index + 1] &&
+          land < array[index + 2])
+      ) {
+        addValley(land, valleys, valleysList);
+      }
+    });
+
+    this.printValleys = "ValleysList: " + valleysList + " Total of valleys: " + valleysList.length;
+    this.cdRef.detectChanges();
+  };
+
 }
